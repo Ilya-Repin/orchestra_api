d96a169054d50aad866a3ccf2b74ccec6a448567
Ilya Repin <ilya-r04@yandex.ru>
2025-04-25 01:35:21 +0500

    Fix GetRegisteredEvents

--- internal/infra/storage/postgres/postgres.go
+++ internal/infra/storage/postgres/postgres.go
@@ -479,7 +479,7 @@ func (s *PostgresStorage) GetRegisteredEvents(ctx context.Context, memberID uuid
 		AND e.id IN (
 			SELECT reg.event_id
 			FROM registrations reg
-			WHERE reg.user_id = $1
+			WHERE reg.user_id = $1 AND reg.registration_status = 'registered'
 		)
 		ORDER BY e.event_date ASC;
 	`
